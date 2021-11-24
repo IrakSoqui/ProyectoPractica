70ffe91337ada2fcd82a6133610887b9ab41c671
Irak Darek Ortiz Soqui <37721867+pirer48@users.noreply.github.com>
2021-11-24 12:02:45 -0700

    first commit

---
+++ src/app/pages/checkout/thank-you-page/thank-you-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thank-you-page',
+  template: `
+  <div class="container">
+  <h1 class="title">Thank You!!</h1>
+  <p class="content">Your Order in on the way!</p>
+  </div>`,
+  styleUrls: ['./thank-you-page.component.scss']
+})
+export class ThankYouPageComponent  {
+
+}
